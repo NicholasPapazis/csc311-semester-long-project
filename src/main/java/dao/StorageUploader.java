2c4182dc7475bd8346d26ce453f172d29141cf54
NicholasPapazis <nicholasepapazis@gmail.com>
2024-11-14 12:56:25 -0500

    connected local project to azure backend

---
+++ src/main/java/dao/StorageUploader.java
@@ -0,0 +1,26 @@
+package dao;
+
+import com.azure.storage.blob.BlobClient;
+import com.azure.storage.blob.BlobClientBuilder;
+import com.azure.storage.blob.BlobContainerClient;
+import com.azure.storage.blob.BlobContainerClientBuilder;
+
+public class StorageUploader {
+
+    private BlobContainerClient containerClient;
+
+    public StorageUploader( ) {
+        this.containerClient = new BlobContainerClientBuilder()
+                .connectionString("DefaultEndpointsProtocol=https;AccountName=papaziscsc311storage;AccountKey=nc0+nuja2oz29/aipE90NAJ+lPGb3axepOmfg5wlTy2VCOPztc/7ljSn6GCNnyyU8C/xswFlH3fe+AStx0ybNg==;EndpointSuffix=core.windows.net")
+                .containerName("media-files")
+                .buildClient();
+    }
+
+    public void uploadFile(String filePath, String blobName) {
+        BlobClient blobClient = containerClient.getBlobClient(blobName);
+        blobClient.uploadFromFile(filePath);
+    }
+    public BlobContainerClient getContainerClient(){
+        return containerClient;
+    }
+}
